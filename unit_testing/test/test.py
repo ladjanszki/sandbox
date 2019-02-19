dd0d0d1815131ce33406f5db2aacc4a76ce12c1e
István Ladjánszki <istvan.ladjanszki@gmail.com>
2019-02-19 15:22:07 +0100

    * Some unit testing example

---
+++ unit_testing/test/test.py
@@ -0,0 +1,16 @@
+import unittest
+from my_sum import sum
+
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Testing my own sum function on integer list
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+if __name__ == "__main__":
+    unittest.main()
